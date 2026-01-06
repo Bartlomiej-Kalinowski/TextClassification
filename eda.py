a24bd4e41ae9a148103205c413a65af17ac40712
Bartlomiej-Kalinowski <kalinowskibartlomiej04@gmail.com>
2026-01-06 18:48:01 +0100

    First EDA(datasets explorartory analysis) features added

---
+++ eda.py
@@ -0,0 +1,50 @@
+from dataLoader import Loader
+import pandas as pd
+import os
+
+class Eda:
+    def __init__(self, path_to_downoladed_train_dataset, sep = ','):
+        self.dataset = pd.read_csv(path_to_downoladed_train_dataset, encoding='utf-8', sep = sep)
+
+    def display_info_about_dataset(self):
+        print(self.dataset.shape)
+        print(self.dataset.dtypes)
+        print(self.dataset.head(3))
+        print(self.dataset.tail(3))
+        print(self.dataset.count())
+
+    def dr_duplicates(self):
+        duplicate_rows = self.dataset[self.dataset.duplicated()]
+        print(self.dataset[self.dataset.duplicated()].head(5))
+        print("number of duplicate rows: ", duplicate_rows.shape[0])
+        df = self.dataset.drop_duplicates()
+        df.count()
+
+def main():
+    ld = Loader(
+        'kaggle',
+        "ashfakyeafi/spam-email-classification",  # dataset 1 - spam ham emails classification
+        "lakshmi25npathi/imdb-dataset-of-50k-movie-reviews",  # dataset 2 - movie reviews sentiment classification
+        "aadyasingh55/fake-news-classification"  # fake_news classification
+    )
+    ld.load()
+
+    print("\n\nDataset emails spam ham:\n\n")
+    ds_emails = Eda(os.path.join(ld.download_paths[0], 'email.csv'))
+    ds_emails.display_info_about_dataset()
+    ds_emails.dr_duplicates()
+
+    print("\n\nDataset movie reviews sentiment:\n\n")
+    ds_sentiment = Eda(os.path.join(ld.download_paths[1], 'IMDB Dataset.csv'))
+    ds_sentiment.display_info_about_dataset()
+    ds_sentiment.dr_duplicates()
+
+    print("\n\nDataset fake news:\n\n")
+    ds_fake_news = Eda(os.path.join(ld.download_paths[2], 'train (2).csv'), sep = ';')
+    ds_fake_news.display_info_about_dataset()
+    ds_fake_news.dr_duplicates()
+
+
+if __name__ == '__main__':
+    main()
+
